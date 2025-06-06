095f3943923a7b4532207cb81201909b27d12eb1
OpexRah <dougisrave@gmail.com>
2025-06-06 19:42:09 +0530

    fix: carousel order

--- src/components/aboutus_page/TeamTabContent.tsx
+++ src/components/aboutus_page/TeamTabContent.tsx
@@ -309,8 +309,10 @@ const TeamTabContent = () => {
             <CarouselNext className="static translate-y-0" />
           </div>
           <CarouselContent className="-ml-4">
-            {team.map((el, index) => (
-              <TeamCarouselItem {...el} key={index} />
+            {[...team]
+              .sort((a, b) => b.year - a.year) // Sort by year descending
+              .map((el, index) => (
+                <TeamCarouselItem {...el} key={index} />
             ))}
           </CarouselContent>
         </Carousel>
